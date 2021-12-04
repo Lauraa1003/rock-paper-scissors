c218714b34cb5f342d98f62f2810c8ba2c883c2f
Lauraa1003 <88458945+Lauraa1003@users.noreply.github.com>
2021-12-04 10:55:45 +0100

    add game function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let playerScore = 0
+let computerScore = 0
+    
     // create a function that returns random elements
     function computerPlay() {
-        const choices = ["rock ", "paper ", "scissors "];
+        const choices = ["rock", "paper", "scissors"];
         let random = choices[Math.floor(Math.random() * choices.length)];
         return random;
         }
@@ -10,36 +13,50 @@
             let computerSelection = computerPlay()
             let playerSelection = prompt('What is your choice?').toLowerCase();
             let result = ""
+        // if playerSelection wins add score to playerScore and show result
             if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
                 (playerSelection == 'paper' && computerSelection == 'rock') ||
                 (playerSelection == 'scissors' && computerSelection == 'paper')) {
+
+                    playerScore++
     
-                    result = ( 'You win! ' + playerSelection + ' beats ' + computerSelection)
+                    result = ( 'You win! ' + playerSelection + ' beats ' + computerSelection + ' Your score is ' + playerScore + ' computer score is ' + computerScore)
+                    
+                   
                 }
-    
-            else if ( playerSelection === computerSelection) {
+        // if same choice show tie result
+            else if (playerSelection === computerSelection) {
                 
                 result = (" it's a tie! You both chose " + computerSelection)
             }
-    
+        
+        // if computerSelection wins add score to ComputerScore
             else if ((computerSelection == 'rock' && playerSelection == 'scissors') ||
                  (computerSelection == 'paper' && playerSelection == 'rock') ||
                  (computerSelection == 'scissors' && playerSelection == 'paper'))
              {
-    
-                result = (' You lose! ' + computerSelection + ' beats ' + playerSelection)
+                computerScore++
+                result = (' You lose! ' + computerSelection + ' beats ' + playerSelection + ' Your score is ' + playerScore + ' computer score is ' + computerScore)
+
+               
             }
-    
-            return result;
+             console.log(result);
         
-    
         }
-    
-        playRound();
-    
+        
+       // loop over the game 5 times 
         function game() {
-            
+            for( i = 0; i < 5; i++ ) { 
+                playRound();
+            }
+       // count score and show winner/loser/tie
+            if (playerScore > computerScore) {
+                console.log(' Yay you won the game! ');
+            } else if (computerScore > playerScore) {
+                console.log(' Too bad the computer won this game ');
+            } else if (computerScore === playerScore) {
+                console.log(' It\'s a tie! ')
+            }
         }
-    
-     
+        game();
     
