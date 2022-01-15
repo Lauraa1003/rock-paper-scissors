f502008e985c45191db7dcc1f7e7ab39a5b9fb39
Lauraa1003 <88458945+Lauraa1003@users.noreply.github.com>
2022-01-15 17:00:22 +0100

    added results container

--- script.js
+++ script.js
@@ -7,37 +7,49 @@ const playerChoiceIcon = document.querySelector('#player-choice');
 const computerChoiceIcon = document.querySelector('#computer-choice');
 const roundResult = document.querySelector('#round-result');
 
+
+
+// hide start container
+document.getElementById("startbutton").addEventListener("click", hideDiv);
+let startContainer = document.getElementById('startcontainer');
+
+function hideDiv() {
+    startContainer.style.display = 'none';
+}
+
+
+
+ // computer chooses random element from array
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    let random = choices[Math.floor(Math.random() * choices.length)];
+    return random;
+ }
     
-    // computer chooses random element from array
-    function computerPlay() {
-        const choices = ["rock", "paper", "scissors"];
-        let random = choices[Math.floor(Math.random() * choices.length)];
-        return random;
-        }
-    
-        // create a function that plays a single round of Rock Paper Scissors
-        function playRound(playerSelection, computerSelection) {
-            let result = ""
-            if (playerScore < 5 && computerScore < 5){
-                if(playerSelection == 'rock'){
-                    playerChoiceIcon.className = 'fas fa-hand-rock';
-                }
-                else if (playerSelection == 'paper') {
-                    playerChoiceIcon.className = 'fas fa-hand-paper';
-                }
-                else if (playerSelection == 'scissors') {
-                    playerChoiceIcon.className = 'fas fa-hand-scissors';
-                }
-                if (computerSelection == 'rock') {
-                    computerChoiceIcon.className = 'fas fa-hand-rock';
-                }
-                else if (computerSelection == 'paper') {
-                    computerChoiceIcon.className = 'fas fa-hand-paper';
-                }
-                else if (computerSelection == 'scissors') {
-                    computerChoiceIcon.className = 'fas fa-hand-scissors';
-                }
-        // if playerSelection wins add score to playerScore and show result
+// create a function that plays a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+    let result = ""
+         if (playerScore < 5 && computerScore < 5){
+          if(playerSelection == 'rock'){
+             playerChoiceIcon.className = 'fas fa-hand-rock';
+            }
+         else if (playerSelection == 'paper') {
+             playerChoiceIcon.className = 'fas fa-hand-paper';
+            }
+         else if (playerSelection == 'scissors') {
+             playerChoiceIcon.className = 'fas fa-hand-scissors';
+            }
+          if (computerSelection == 'rock') {
+             computerChoiceIcon.className = 'fas fa-hand-rock';
+            }
+         else if (computerSelection == 'paper') {
+             computerChoiceIcon.className = 'fas fa-hand-paper';
+            }
+         else if (computerSelection == 'scissors') {
+             computerChoiceIcon.className = 'fas fa-hand-scissors';
+            }
+
+//if playerSelection wins add score to playerScore and show result
             if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
                 (playerSelection == 'paper' && computerSelection == 'rock') ||
                 (playerSelection == 'scissors' && computerSelection == 'paper')) {
@@ -45,15 +57,15 @@ const roundResult = document.querySelector('#round-result');
                  roundResult.textContent = "You won this round!";
                  playerScore++
                  playerCurrentScore.textContent = playerScore;
+            }
 
-                }
-        // if same choice show tie result
+// if same choice show tie result
             else if (playerSelection === computerSelection) {
                 
                 roundResult.textContent = "It's a tie!";
             }
         
-        // if computerSelection wins add score to ComputerScore
+// if computerSelection wins add score to ComputerScore
             else if ((computerSelection == 'rock' && playerSelection == 'scissors') ||
                  (computerSelection == 'paper' && playerSelection== 'rock') ||
                  (computerSelection == 'scissors' && playerSelection == 'paper')){
@@ -68,31 +80,50 @@ const roundResult = document.querySelector('#round-result');
              computerCurrentScore.textContent = computerScore;
              resultPopup();
             }
+
         }
     }
-    
+
+    function showGameResults() {
+        let gameContainer = document.querySelector('.game1');
+        let bottomContainer = document.getElementById('bottomcontainer');
+        gameContainer.style.display = 'none';
+        bottomContainer.style.display = 'block';
+    }
+
 
  const loseSound =document.getElementById("lose");   
  const winSound = document.getElementById("win");  
-
+ const div = document.createElement('div');  
+ const container = document.querySelector('.bottomcontainer');
+ const content = document.createElement('div');
  
-    function resultPopup(){
+
+
+ // show results + winner/loser sound + text content.
+function resultPopup(){
         if(playerScore == 5){
-            roundResult.textContent = "YOU ARE THE WINNER!"
+            showGameResults()
+            content.classList.add('content');
+            content.textContent = `Yayyy you win!\nYour score is: ${playerScore}\nand the computer score is: ${computerScore}!`;
+            container.appendChild(content);
             if (!winSound) return;
             winSound.currentTime = 0;
             winSound.play(); 
            
         }
         else if (computerScore == 5){
-            roundResult.textContent = "TOO BAD THE COMPUTER WINS"
+            showGameResults()
+            content.classList.add('content');
+            content.textContent = `Too bad, the computer wins this time!\nThe computer score is:${computerScore}\nand your score is:${playerScore}!`;
+            container.appendChild(content);
             if (!loseSound) return;
             loseSound.currentTime = 0;
             loseSound.play(); 
         }
     }
     
-        
+
 const rockBtn = document.querySelector('#rock-button');
 const paperBtn = document.querySelector('#paper-button');
 const scissorsBtn = document.querySelector('#scissors-button');
@@ -126,32 +157,30 @@ const scissorsBtn = document.querySelector('#scissors-button');
             mySound.currentTime = 0;
             mySound.play(); });         
 
-const startBtn = document.getElementById("start");
+document.getElementById("restart").addEventListener("click", restartGame);
 
-//create start button that activates game + sound
-const startSound = document.getElementById("play");
+function restartGame(){
+    location.reload();
 
-
-
-
+}
 
 
 
 
 
+// previous code / console.log
 
-
-   // loop over the game 5 times 
+// loop over the game 5 times 
        /*
         function game() {
             for( i = 0; i < 5; i++ ) { 
                 playRound();
             }
-            */
+            
 
-            /*
+            
            
-       // count score and show winner/loser/tie
+// count score and show winner/loser/tie
             if (playerScore > computerScore) {
                 console.log(' Yay you won the game! ');
             } else if (computerScore > playerScore) {
@@ -163,4 +192,4 @@ const startSound = document.getElementById("play");
         game();
     */
 
-    
+   
